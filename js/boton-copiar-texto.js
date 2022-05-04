e256680aedfb01104c946e79625b7b69f1fa6e4f
MauroPeralta1595 <101117763+MauroPeralta1595@users.noreply.github.com>
2022-05-04 13:26:09 -0300

    Actualización de botón copiar
    Se modificó el archivo solucionando el error de que al copiar se agregaba una linea /n no deseada. Además se separó la función específica del copiado con la función del botón copiar

--- js/boton-copiar-texto.js
+++ js/boton-copiar-texto.js
@@ -6,18 +6,14 @@ let clase2 = 'aviso-texto-copiado-visible';
 botonCopiar.addEventListener('click', function (event) {
     event.preventDefault();
     let codigoACopiar = document.querySelector('.texto-salida');
-    let seleccion = document.createRange(); //creo el rango de selección de mi botón copiar
-    seleccion.selectNodeContents(codigoACopiar); //elijo el contenido a copiar
-    window.getSelection().removeAllRanges(); //remuevo selecciones anteriores
-    window.getSelection().addRange(seleccion); //agrego mi rango de selección
-    var res = document.execCommand('copy'); //copio lo que esté en el rango de selección
-    window.getSelection().removeRange(seleccion); // limpio el rango de selección de lo que el botón copia
+    copiarAlPortapapeles(codigoACopiar); //ejecuto mi función para copiar
     aparecerDesaparecerAviso(clase1, clase2); //ejecuto mi funcion para que aparezca el aviso de texto copiado
     setTimeout(function () { // con esta función, el aviso de texto copiado desaparece lentamente
         avisoTextoCopiado.classList.replace(clase2, clase1);
     }, 1300);
-})
+    
+});
 
 function aparecerDesaparecerAviso(clase1, clase2) { //función para visibilizar u ocultar el aviso de texto copiado
-    avisoTextoCopiado.classList.replace(clase1, clase2);
+  avisoTextoCopiado.classList.replace(clase1, clase2);
 }
